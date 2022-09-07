6bd5166bd854be0faf28cdb588b8f85d6cc54ad7
Han4578 <99018603+Han4578@users.noreply.github.com>
2022-09-07 17:06:19 +0800

    added winner system

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let blueTurn = true
 
 for (let i = 0; i < 9; i++) {
     let box = document.createElement('div')
-    box.id = i
     box.classList.add('boxes')
+    box.dataset.colour = 'none'
     container.appendChild(box)
     boxes.push(box)
 }
@@ -21,7 +21,7 @@ boxes.forEach(box => {
         options.forEach(o => {
             o.style.pointerEvents = 'none'
         })
-    
+
 
         let image = document.createElement('img')
         let c
@@ -29,46 +29,51 @@ boxes.forEach(box => {
         if (Array.from(e.target.children).length == 0) {
             if (blueTurn && blueBig) {
                 image.src = 'images/bigRing.png'
-                image.dataset.colour = 'blue'
                 c = 'blue'
             } else if (blueTurn && !blueBig) {
                 image.src = 'images/smallRing.png'
-                image.dataset.colour = 'blue'
                 c = 'blue'
             } else if (!blueTurn && redBig) {
                 image.src = 'images/bigCross.png'
-                image.dataset.colour = 'red'
                 c = 'red'
             } else {
                 image.src = 'images/smallCross.png'
-                image.dataset.colour = 'red'
                 c = 'red'
             }
             e.target.appendChild(image)
             changeSize(c)
             changeTurn()
+            e.target.dataset.colour = c
+
         } else {
             let newSrc = e.target.firstChild.src
             if (e.target.firstChild.src.indexOf('images/bigRing.png') != -1 && blueTurn && !blueBig) {
                 newSrc = 'images/ringInRing.png'
+                a = 'blue'
                 c = 'blue'
             } else if (e.target.firstChild.src.indexOf('images/bigCross.png') != -1 && blueTurn && !blueBig) {
                 newSrc = 'images/ringInCross.png'
+                a = 'blue red'
                 c = 'blue'
             } else if (e.target.firstChild.src.indexOf('images/bigCross.png') != -1 && !blueTurn && !redBig) {
                 newSrc = 'images/crossInCross.png'
+                a = 'red'
                 c = 'red'
             } else if (e.target.firstChild.src.indexOf("images/bigRing.png") != -1 && !blueTurn && !redBig) {
                 newSrc = 'images/crossInRing.png'
+                a = 'blue red'
                 c = 'red'
             }
 
-            if(e.target.firstChild.src !== newSrc) {
-            e.target.firstChild.src = newSrc
-            changeSize(c)
-            changeTurn()
+            if (e.target.firstChild.src !== newSrc) {
+                e.target.firstChild.src = newSrc
+                changeSize(c)
+                changeTurn()
+                e.target.dataset.colour = a
             }
         }
+
+        checkForWin()
     })
 })
 redSide.forEach(size => {
@@ -82,7 +87,7 @@ first.forEach(size => {
 })
 
 function changeSize(colour) {
-    
+
     if (colour == 'blue') {
         blueBig = !blueBig
         blueSide.forEach(size => {
@@ -115,5 +120,42 @@ function changeTurn() {
         } else if (!blueTurn && c.classList.contains('redFirst')) {
             c.classList.add('selected')
         }
+    })
+}
+
+function checkForWin() {
+    let combinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    let colour = ['red', 'blue']
+
+    colour.forEach(c => {
+        let colour = c
+
+        combinations.forEach(combo => {
+            let a = boxes[combo[0]]
+            let b = boxes[combo[1]]
+            let c = boxes[combo[2]]
+
+            if (a.dataset.colour.toString().includes(colour) && b.dataset.colour.toString().includes(colour) && c.dataset.colour.toString().includes(colour)){
+                declareWinner(colour)
+            }
+        })
+    })
+}
+
+function declareWinner(c) {
+    boxes.forEach(box => {
+        box.style.pointerEvents = 'none'
+        let winner = document.querySelector('#winner')
+        winner.innerHTML = 'Winner: ' + c
+        winner.style.display = 'block'
     })
 }
